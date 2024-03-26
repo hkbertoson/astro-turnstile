db006c821deacf22ad29e177cc36605fe3a2fe26
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-03-26 13:36:30 -0500

    🚀 Add Astro Turnstile integration setup
    Added Astro Turnstile integration setup with options validation, route injection, and virtual imports handling. Ensure TURNSTILE_TOKEN is provided in .env.

--- package/src/integration.ts
+++ package/src/integration.ts
@@ -1,8 +1,52 @@
-import type { AstroIntegration } from "astro";
+import { defineIntegration,createResolver, addVirtualImports } from "astro-integration-kit"
+import {z} from 'astro/zod'
+import {loadEnv} from 'vite'
 
-export const integration = (): AstroIntegration => {
-	return {
-		name: "package-name",
-		hooks: {},
-	};
-};
+const {TURNSTILE_TOKEN} = loadEnv("all", process.cwd(), "TURNSTILE_")
+if(!TURNSTILE_TOKEN){
+	throw new Error("Missing TURNSTILE_TOKEN in .env")
+}
+
+
+
+export default defineIntegration({
+    name: "astro-turnstile",
+    optionsSchema: z.object({siteKey: z.string()}),
+    setup({options}){
+        const {resolve} = createResolver(import.meta.url);
+
+		if(!options.siteKey){
+			throw new Error("Missing siteKey in options")
+		}
+        return({
+            "astro:config:setup"(params){
+                const {injectRoute} = params
+                injectRoute({
+                    pattern: "/verify",
+                    entrypoint: resolve("verify.ts")
+                }),
+                addVirtualImports(params,{
+                    name: 'virtual:astro-turnstile/config',
+                    imports: {
+                        'virtual:astro-turnstile/config': `export default ${JSON.stringify({options, TURNSTILE_TOKEN})}`,
+                    }
+                })
+            }
+        })
+    }
+
+})
+
+// export const integration = (): AstroIntegration => {
+// 	return {
+// 		name: "astro-turnstile",
+// 		hooks: {
+// 			"astro:config:setup"({config,logger,injectRoute}){
+// 				injectRoute({
+// 					pattern: "/verify",
+// 					entrypoint: "astro-turnstile/verify.astro"
+// 				})
+// 			}
+// 		},
+// 	};
+// };
